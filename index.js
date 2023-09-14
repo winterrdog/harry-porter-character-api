10cb3524611309a4d59218e9e099389b379f9e9e
winterrdog <winterrdog@protonmail.ch>
2023-09-14 11:45:04 +0300

    added `routes`

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 const express = require("express");
-const app = express();
+const { Utility } = require("./utility");
 
 const port = process.env.PORT ?? 3000;
+const app = express();
+
+// fetch all characters
+app.get("/hogwarts/characters", async function (req, res, next) {
+    try {
+        const characters = await Utility.fetchCharacters();
+
+        if (characters.length === 0) throw new Error("data unavailable");
+
+        return res.status(200).json({
+            fromCache: false,
+            data: characters,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(404).send("data unavailable");
+    }
+});
+
+// fetch single character
+app.get("/hogwarts/characters/:id", async function (req, res, next) {
+    try {
+        const character = await Utility.fetchCharacters(req.params.id);
+
+        if (character.length === 0) throw new Error("data unavailable");
+
+        return res.status(200).json({
+            fromCache: false,
+            data: character,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(404).send("data unavailable");
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
